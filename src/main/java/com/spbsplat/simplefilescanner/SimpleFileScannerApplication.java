4577896b6490a047c770e9963d4dc97751d0abee
Tifider <Tifider@gmail.com>
2019-06-25 14:32:08 +0300

    Initial commit

---
+++ src/main/java/com/spbsplat/simplefilescanner/SimpleFileScannerApplication.java
@@ -0,0 +1,13 @@
+package com.spbsplat.simplefilescanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleFileScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleFileScannerApplication.class, args);
+	}
+
+}
